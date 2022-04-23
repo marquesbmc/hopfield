17a2b5acfd4fd8df1e010bc0145251e04f0934d1
Bruno Marques <marquesbmc@gmail.com>
2022-04-23 19:57:49 -0300

    first commit

---
+++ HopfiledNetwork.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def activation_function(x):
+    if x < 0:
+        return -1
+    return 1
+
+
+class Matrix:
+
+    @staticmethod
+    def matrix_vector_multiplication(matrix, vector):
+        return matrix.dot(vector)
+    
+    @staticmethod
+    def clear_diagonal(matrix):
+        np.fill_diagonal(matrix, 0)
+        return matrix
+    
+    @staticmethod
+    def outer_product(pattern):
+        return np.outer(pattern, pattern)
+    
+    @staticmethod
+    def add_matrices(matrix1, matrix2):
+        return matrix1 + matrix2
+    
+    
+class HopfieldNetwork:
+    
+    def __init__(self, dimension):
+        self.weight_matrix = np.zeros((dimension, dimension))
+
+    def train(self, pattern):
+        pattern_bipolar = HopfieldNetwork.transform(pattern)
+        
+        pattern_weight_matrix = Matrix.outer_product(pattern_bipolar)
+        
+        pattern_weight_matrix = Matrix.clear_diagonal(pattern_weight_matrix)
+        
+        self.weight_matrix = Matrix.add_matrices(self.weight_matrix, pattern_weight_matrix)
+        
+    def recall(self, pattern):
+        pattern_bipolar = HopfieldNetwork.transform(pattern)
+      
+        result = Matrix.matrix_vector_multiplication(self.weight_matrix, pattern_bipolar)
+        result = np.array([activation_function(x) for x in result])
+        result = HopfieldNetwork.re_transform(result)
+        
+        print(result)
+
+    @staticmethod
+    def transform(pattern):
+        return np.where(pattern == 0, -1, pattern)
+    
+    @staticmethod
+    def re_transform(pattern):
+        return np.where(pattern == -1, 0, pattern)
+
+
+if __name__ == '__main__':
+    
+    network = HopfieldNetwork(dimension=4)
+    network.train(np.array([1, 1, 1, 1]))
+    network.recall(np.array([1, 0, 1, 0]))
+    
